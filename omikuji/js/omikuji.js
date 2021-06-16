2d062c41c76d6cf14e40a017fbde30da664c7591
nguyenthangJRS <nguyenthangJRS@gmail.com>
2021-06-16 21:31:09 +0900

    update omikuji

--- omikuji/js/omikuji.js
+++ omikuji/js/omikuji.js
@@ -21,31 +21,50 @@ window.addEventListener('DOMContentLoaded',function(){
     window.alert(popmsg)
 },false);
 
-
+/////////////////////////////////////////// 
+const arr = ["大吉!!!!!","吉!!!!","中吉!!!","小吉!!","末吉!","凶。。"];
 const btn = document.getElementById('btn');
-btn.addEventListener('click',function(){
-    let n = Math.floor(Math.random()*3);
-    switch(n){
-        case 0:{
-            btn.textContent = "Very Happy!!";
-            btn.style.color = "#ff0000";
-            btn.style.fontSize = "40px";
-            btn.style.lineHeight = "50px";
-            break;
+const result = document.querySelector('.result');
+
+const type = (fontSise,color,number) => {
+            result.style.fontSize = `${fontSise}px`;
+            result.style.background = `${color}`;
+            result.textContent = arr[number]
+}
+btn.addEventListener('click',function(e){
+    let n = Math.floor(Math.random()*6);
+    e.target.style.animation = `result 1.5s linear `;
+    const time = setTimeout (function(){
+        switch(n){
+            case 0 : {
+                type(40,'#c0392b',0);
+                break;
+            }
+            case 1 : {
+                type(30,'#e67e22',1);
+                break;
+            }
+            case 2 : {
+                type(20,'#f1c40f',2);
+                break;
+            }
+            case 3 : {
+                type(20,'#16a085',3);
+                break;
+            }
+            case 4 : {
+                type(20,'#3498db',4);
+                break;
+            }
+            case 5 : {
+                type(20,'#34495e',5);
+                break;
+            }
         }
-        case 1:{
-            btn.textContent = "Happy!!";
-            btn.style.color = "#ff0001";
-            btn.style.fontSize = "30px";
-             break;
-        }
-        case 2:{
-            btn.textContent = "UnHappy!!";
-            btn.style.color = "#261e1c";
-            btn.style.fontSize = "20px";
-             break;
-        }
-    }
+        e.target.style.animation = `none `;
+
+    },2000);
+
     // snowfall stop
     $(document).snowfall('clear');
     // snowfall
@@ -57,6 +76,21 @@ btn.addEventListener('click',function(){
             minSize : 1,
             image : 'img/sakura_hanabira.png'
         })
+        $(document).snowfall({
+            maxSpeed : 5,
+            minSpeed : 1,
+            maxSize : 20,
+            minSize : 1,
+            image : 'img/snowflakes.png'
+        })
+        $(document).snowfall({
+            maxSpeed : 5,
+            minSpeed : 1,
+            maxSize : 20,
+            minSize : 1,
+            image : 'img/star.png'
+        })
     })
+  
 },false)
 
